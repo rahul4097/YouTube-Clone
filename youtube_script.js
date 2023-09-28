9a15320b28627bf66a094c8c75e64136d3087f3d
Rahul kumar <138388064+rahul4097@users.noreply.github.com>
2023-09-28 21:52:55 +0530

    Add files via upload

---
+++ youtube_script.js
@@ -0,0 +1,6 @@
+// const toggle=document.getElementById('effect');
+// const bar=document.getElementsByClassName('side-bar');
+
+// toggle.addEventListener('click',()=>{
+//     bar.classList.toggle('close');
+// });
